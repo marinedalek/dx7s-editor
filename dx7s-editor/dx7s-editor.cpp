fa64351859a6c07c0c0201791a35b99b5c36e59a
marinedalek <marinedalek@gmail.com>
2023-08-10 04:25:20 +0100

    Add project files.

---
+++ dx7s-editor/dx7s-editor.cpp
@@ -0,0 +1,12 @@
+﻿// dx7s-editor.cpp : Defines the entry point for the application.
+//
+
+#include "dx7s-editor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
