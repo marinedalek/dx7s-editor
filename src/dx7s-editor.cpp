f3c42281dc25b4264b7bebf2f85f082da64e034a
marinedalek <marinedalek@gmail.com>
2023-08-23 22:52:52 +0100

    Initial GLFW/Imgui commit

---
+++ src/dx7s-editor.cpp
@@ -0,0 +1,84 @@
+﻿// dx7s-editor.cpp : Defines the entry point for the application.
+//
+
+#include "dx7s-editor.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <glad/glad.h>
+#define GL_SILENCE_DEPRECATION
+#include <GLFW/glfw3.h>
+#include <print>
+
+static void glfw_error_callback(int error, const char* description)
+{
+	std::println(stderr, "GLFW Error {}: {}", error, description);
+}
+
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main(int argc, char* argv[])
+{
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit()) {
+		return -1;
+	}
+	const char* glsl_version = "#version 330";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "DX7S Editor", NULL, NULL);
+	if (window == NULL) {
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::println(stderr, "Failed to initialize GLAD");
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	bool show_demo_window = true;
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::ShowDemoWindow(&show_demo_window);
+		ImGui::Render();
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
